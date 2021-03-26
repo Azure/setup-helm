4057e7396e0dc0bb44eebd6a048ba75d2f2a38a3
Sundar Guntnur <sundargs2000@github.com>
2021-03-26 21:35:41 +0530

    Added trigger for L2 tests and ran build.

---
+++ .github/workflows/TriggerIntegrationTests.sh
@@ -0,0 +1,32 @@
+token=$1
+commit=$2
+repository=$3
+prNumber=$4
+frombranch=$5
+tobranch=$6
+
+getPayLoad() {
+    cat <<EOF
+{
+    "event_type": "SetupHelmActionPR", 
+    "client_payload": 
+    {
+        "action": "SetupHelm", 
+        "commit": "$commit", 
+        "repository": "$repository", 
+        "prNumber": "$prNumber", 
+        "tobranch": "$tobranch", 
+        "frombranch": "$frombranch"
+    }
+}
+EOF
+}
+
+response=$(curl -X POST -H "Authorization: token $token" https://api.github.com/repos/sundargs2000/azure-actions-integration-tests/dispatches --data "$(getPayLoad)")
+
+if [ "$response" == "" ]; then
+    echo "Integration tests triggered successfully"
+else
+    echo "Triggering integration tests failed with: '$response'"
+    exit 1
+fi
