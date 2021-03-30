4420cb2ff7c9d3390f4a79d1f1c95dca30bc9009
Shivam Gupta <shigupt@microsoft.com>
2021-03-30 21:22:57 +0530

    Changing feature flag name

--- src/run.ts
+++ src/run.ts
@@ -21,24 +21,24 @@ const LATEST_HELM3_VERSION = '3.*';
 const helmAllReleasesUrl = 'https://api.github.com/repos/helm/helm/releases';
 
 function getExecutableExtension(): string {
-  if (os.type().match(/^Win/)) {
-    return '.exe';
-  }
-  return '';
+    if (os.type().match(/^Win/)) {
+        return '.exe';
+    }
+    return '';
 }
 
 function getHelmDownloadURL(version: string): string {
-  switch (os.type()) {
-    case 'Linux':
-      return util.format('https://get.helm.sh/helm-%s-linux-amd64.zip', version);
+    switch (os.type()) {
+        case 'Linux':
+            return util.format('https://get.helm.sh/helm-%s-linux-amd64.zip', version);
 
-    case 'Darwin':
-      return util.format('https://get.helm.sh/helm-%s-darwin-amd64.zip', version);
+        case 'Darwin':
+            return util.format('https://get.helm.sh/helm-%s-darwin-amd64.zip', version);
 
-    case 'Windows_NT':
-    default:
-      return util.format('https://get.helm.sh/helm-%s-windows-amd64.zip', version);
-  }
+        case 'Windows_NT':
+        default:
+            return util.format('https://get.helm.sh/helm-%s-windows-amd64.zip', version);
+    }
 }
 
 export async function getStableHelmVersion(): Promise<string> {
@@ -67,45 +67,45 @@ export async function getStableHelmVersion(): Promise<string> {
 }
 
 var walkSync = function (dir, filelist, fileToFind) {
-  var files = fs.readdirSync(dir);
-  filelist = filelist || [];
-  files.forEach(function (file) {
-    if (fs.statSync(path.join(dir, file)).isDirectory()) {
-      filelist = walkSync(path.join(dir, file), filelist, fileToFind);
-    }
-    else {
-      core.debug(file);
-      if (file == fileToFind) {
-        filelist.push(path.join(dir, file));
-      }
-    }
-  });
-  return filelist;
+    var files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function (file) {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            filelist = walkSync(path.join(dir, file), filelist, fileToFind);
+        }
+        else {
+            core.debug(file);
+            if (file == fileToFind) {
+                filelist.push(path.join(dir, file));
+            }
+        }
+    });
+    return filelist;
 };
 
 async function downloadHelm(version: string): Promise<string> {
-  if (!version) { version = await getLatestHelmVersionFor("v3"); }
-  let cachedToolpath = toolCache.find(helmToolName, version);
-  if (!cachedToolpath) {
-    let helmDownloadPath;
-    try {
-      helmDownloadPath = await toolCache.downloadTool(getHelmDownloadURL(version));
-    } catch (exception) {
-      throw new Error(util.format("Failed to download Helm from location ", getHelmDownloadURL(version)));
+    if (!version) { version = await getLatestHelmVersionFor("v3"); }
+    let cachedToolpath = toolCache.find(helmToolName, version);
+    if (!cachedToolpath) {
+        let helmDownloadPath;
+        try {
+            helmDownloadPath = await toolCache.downloadTool(getHelmDownloadURL(version));
+        } catch (exception) {
+            throw new Error(util.format("Failed to download Helm from location ", getHelmDownloadURL(version)));
+        }
+
+        fs.chmodSync(helmDownloadPath, '777');
+        const unzipedHelmPath = await toolCache.extractZip(helmDownloadPath);
+        cachedToolpath = await toolCache.cacheDir(unzipedHelmPath, helmToolName, version);
     }
 
-    fs.chmodSync(helmDownloadPath, '777');
-    const unzipedHelmPath = await toolCache.extractZip(helmDownloadPath);
-    cachedToolpath = await toolCache.cacheDir(unzipedHelmPath, helmToolName, version);
-  }
+    const helmpath = findHelm(cachedToolpath);
+    if (!helmpath) {
+        throw new Error(util.format("Helm executable not found in path ", cachedToolpath));
+    }
 
-  const helmpath = findHelm(cachedToolpath);
-  if (!helmpath) {
-    throw new Error(util.format("Helm executable not found in path ", cachedToolpath));
-  }
-
-  fs.chmodSync(helmpath, '777');
-  return helmpath;
+    fs.chmodSync(helmpath, '777');
+    return helmpath;
 }
 
 async function getLatestHelmVersionFor(type: string): Promise<string> {
@@ -113,14 +113,14 @@ async function getLatestHelmVersionFor(type: string): Promise<string> {
     try {
         const { repository } = await graphql(
             `{
-      repository(name:"helm", owner:"helm") {
-        releases(last: 100)  {
-            nodes {
-              tagName
-            }
-        }
-      }
-    }`,
+                repository(name: "helm", owner: "helm") {
+                    releases(last: 100)  {
+                        nodes {
+                            tagName
+                        }
+                    }
+                }
+            }`,
             {
                 headers: {
                     authorization: `token ${token}`
@@ -152,31 +152,31 @@ function isValidVersion(version: string, type: string): boolean {
 }
 
 function findHelm(rootFolder: string): string {
-  fs.chmodSync(rootFolder, '777');
-  var filelist: string[] = [];
-  walkSync(rootFolder, filelist, helmToolName + getExecutableExtension());
-  if (!filelist) {
-    throw new Error(util.format("Helm executable not found in path ", rootFolder));
-  }
-  else {
-    return filelist[0];
-  }
+    fs.chmodSync(rootFolder, '777');
+    var filelist: string[] = [];
+    walkSync(rootFolder, filelist, helmToolName + getExecutableExtension());
+    if (!filelist) {
+        throw new Error(util.format("Helm executable not found in path ", rootFolder));
+    }
+    else {
+        return filelist[0];
+    }
 }
 
 async function run() {
     let version = core.getInput('version', { 'required': true });
 
-    if (process.env['NEW_VERSION_LOGIC'] == 'true') {
-        if (version.toLocaleLowerCase() === 'latest' || version === LATEST_HELM3_VERSION) {
-            version = await getLatestHelmVersionFor("v3");
-        } else if (version === LATEST_HELM2_VERSION) {
-            version = await getLatestHelmVersionFor("v2");
+    if (process.env['HELM_INSTALLER_LEGACY_VERSIONING'] == 'true') {
+        if (version.toLocaleLowerCase() === 'latest') {
+            version = await getStableHelmVersion();
         } else if (!version.toLocaleLowerCase().startsWith('v')) {
             version = 'v' + version;
         }
     } else {
-        if (version.toLocaleLowerCase() === 'latest') {
-            version = await getStableHelmVersion();
+        if (version.toLocaleLowerCase() === 'latest' || version === LATEST_HELM3_VERSION) {
+            version = await getLatestHelmVersionFor("v3");
+        } else if (version === LATEST_HELM2_VERSION) {
+            version = await getLatestHelmVersionFor("v2");
         } else if (!version.toLocaleLowerCase().startsWith('v')) {
             version = 'v' + version;
         }
@@ -194,13 +194,9 @@ async function run() {
     catch {
         //do nothing, set as output variable
     }
-  }
-  catch {
-    //do nothing, set as output variable
-  }
 
-  console.log(`Helm tool version: '${version}' has been cached at ${cachedPath}`);
-  core.setOutput('helm-path', cachedPath);
+    console.log(`Helm tool version: '${version}' has been cached at ${cachedPath}`);
+    core.setOutput('helm-path', cachedPath);
 }
 
 run().catch(core.setFailed);
